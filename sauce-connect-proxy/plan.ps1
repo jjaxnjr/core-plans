74fdab5dc27ccf58c521e6febebd11f0f15d0d0b
James Jackson <james.jackson2@alaskaair.com>
2019-09-30 12:08:11 -0700

    Powershell file is working.

--- sauce-connect-proxy/plan.ps1
+++ sauce-connect-proxy/plan.ps1
@@ -6,7 +6,7 @@ $pkg_upstream_url="https://wiki.saucelabs.com/display/DOCS/Sauce+Connect+Proxy"
 $pkg_description="Sauce Connect™ is a proxy server that opens a secure connection between a Sauce Labs virtual machine running your browser tests, and an application or website you want to test that's on your local machine or behind a corporate firewall."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://saucelabs.com/downloads/sc-$pkg_version-win32.zip"
-$pkg_shasum="1d908877b04bdd7706d8b2b9caba2066dd146644"
+$pkg_shasum="4b2baaeb32624aa4e60ea4a2ca51f7c5656d476ba29f650a5dabb0faaf6cb793"
 $pkg_filename="sc-$pkg_version-win32.zip"
 $pkg_bin_dirs=@("bin")
 
